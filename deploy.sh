d79e43382aaf6723a163a86f39bd39aa095e9730
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-03-23 15:13:31 +0100

    Renamed repository to 'vue-gh-pages'

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pagesgit push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 # git push -f https://github.com/<USERNAME>/<REPO>.git master:gh-pages
-git push -f https://github.com/beatrizsmerino/demo-vue-cli-gh-pages.git master:gh-pages
+git push -f https://github.com/beatrizsmerino/vue-gh-pages.git master:gh-pages
 
 cd -
