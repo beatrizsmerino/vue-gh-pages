5e88e79a27edd45cbea48037f4a4be88eb3208b3
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-08-05 09:54:16 +0200

    NPM. Use Dart sass: Removed 'node-sass' and added 'sass' npm package

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
 	publicPath: process.env.NODE_ENV === "production" ? "/vue-gh-pages/" : "/",
+	css: {
+		loaderOptions: {
+			sass: {
+				implementation: require('sass')
+			}
+		}
+	},
 };
