85a0c9f2607c3f8632034407cb8bba0db1ff9eba
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2022-01-05 23:48:23 +0100

    REFACTOR. Format code with the npm script 'lint'

--- src/router/index.js
+++ src/router/index.js
@@ -14,11 +14,9 @@ const routes = [
 		path: '/about',
 		name: 'about',
 
-		/*
-		 * route level code-splitting
-		 * this generates a separate chunk (about.[hash].js) for this route
-		 * which is lazy-loaded when the route is visited.
-		 */
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
 		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
 	}
 ];
