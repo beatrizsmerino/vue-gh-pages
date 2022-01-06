32462bcf42daeff67540e5f94d08120e34fcfc41
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2022-01-06 04:37:53 +0100

    REFACTOR. Use the class selector instead of html tag in the 'HelloWorld' component.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,138 +1,168 @@
 <template>
 	<div class="hello">
-		<h1>
+		<h1 class="hello__title">
 			{{ msg }}
 		</h1>
-		<p>
-			For a guide and recipes on how to configure / customize this project,
-			<br >
-			check out the
-			<a
-				href="https://cli.vuejs.org"
-				target="_blank"
-				rel="noopener"
-			>
-				vue-cli documentation
-			</a>
-			.
-		</p>
-		<h3>Installed CLI Plugins</h3>
-		<ul>
-			<li>
+		<div class="hello__description">
+			<p>
+				For a guide and recipes on how to configure / customize this project,
+				<br >
+				check out the
 				<a
-					href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+					class="hello__link"
+					href="https://cli.vuejs.org"
 					target="_blank"
 					rel="noopener"
 				>
-					babel
+					vue-cli documentation
 				</a>
-			</li>
-			<li>
-				<a
-					href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-					target="_blank"
-					rel="noopener"
-				>
-					eslint
-				</a>
-			</li>
-		</ul>
-		<h3>Essential Links</h3>
-		<ul>
-			<li>
-				<a
-					href="https://vuejs.org"
-					target="_blank"
-					rel="noopener"
-				>
-					Core Docs
-				</a>
-			</li>
-			<li>
-				<a
-					href="https://forum.vuejs.org"
-					target="_blank"
-					rel="noopener"
-				>
-					Forum
-				</a>
-			</li>
-			<li>
-				<a
-					href="https://chat.vuejs.org"
-					target="_blank"
-					rel="noopener"
-				>
-					Community Chat
-				</a>
-			</li>
-			<li>
-				<a
-					href="https://twitter.com/vuejs"
-					target="_blank"
-					rel="noopener"
-				>
-					Twitter
-				</a>
-			</li>
-			<li>
-				<a
-					href="https://news.vuejs.org"
-					target="_blank"
-					rel="noopener"
-				>
-					News
-				</a>
-			</li>
-		</ul>
-		<h3>Ecosystem</h3>
-		<ul>
-			<li>
-				<a
-					href="https://router.vuejs.org"
-					target="_blank"
-					rel="noopener"
-				>
-					vue-router
-				</a>
-			</li>
-			<li>
-				<a
-					href="https://vuex.vuejs.org"
-					target="_blank"
-					rel="noopener"
-				>
-					vuex
-				</a>
-			</li>
-			<li>
-				<a
-					href="https://github.com/vuejs/vue-devtools#vue-devtools"
-					target="_blank"
-					rel="noopener"
-				>
-					vue-devtools
-				</a>
-			</li>
-			<li>
-				<a
-					href="https://vue-loader.vuejs.org"
-					target="_blank"
-					rel="noopener"
-				>
-					vue-loader
-				</a>
-			</li>
-			<li>
-				<a
-					href="https://github.com/vuejs/awesome-vue"
-					target="_blank"
-					rel="noopener"
-				>
-					awesome-vue
-				</a>
-			</li>
-		</ul>
+				.
+			</p>
+		</div>
+
+		<div class="hello__info">
+			<h3 class="hello__subtitle">
+				Installed CLI Plugins
+			</h3>
+			<ul class="hello__list">
+				<li class="hello__item">
+					<a
+						class="hello__link"
+						href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+						target="_blank"
+						rel="noopener"
+					>
+						babel
+					</a>
+				</li>
+				<li class="hello__item">
+					<a
+						class="hello__link"
+						href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+						target="_blank"
+						rel="noopener"
+					>
+						eslint
+					</a>
+				</li>
+			</ul>
+		</div>
+
+		<div class="hello__info">
+			<h3 class="hello__subtitle">
+				Essential Links
+			</h3>
+			<ul class="hello__list">
+				<li class="hello__item">
+					<a
+						class="hello__link"
+						href="https://vuejs.org"
+						target="_blank"
+						rel="noopener"
+					>
+						Core Docs
+					</a>
+				</li>
+				<li class="hello__item">
+					<a
+						class="hello__link"
+						href="https://forum.vuejs.org"
+						target="_blank"
+						rel="noopener"
+					>
+						Forum
+					</a>
+				</li>
+				<li class="hello__item">
+					<a
+						class="hello__link"
+						href="https://chat.vuejs.org"
+						target="_blank"
+						rel="noopener"
+					>
+						Community Chat
+					</a>
+				</li>
+				<li class="hello__item">
+					<a
+						class="hello__link"
+						href="https://twitter.com/vuejs"
+						target="_blank"
+						rel="noopener"
+					>
+						Twitter
+					</a>
+				</li>
+				<li class="hello__item">
+					<a
+						class="hello__link"
+						href="https://news.vuejs.org"
+						target="_blank"
+						rel="noopener"
+					>
+						News
+					</a>
+				</li>
+			</ul>
+		</div>
+
+		<div class="hello__info">
+			<h3 class="hello__subtitle">
+				Ecosystem
+			</h3>
+			<ul class="hello__list">
+				<li class="hello__item">
+					<a
+						class="hello__link"
+						href="https://router.vuejs.org"
+						target="_blank"
+						rel="noopener"
+					>
+						vue-router
+					</a>
+				</li>
+				<li class="hello__item">
+					<a
+						class="hello__link"
+						href="https://vuex.vuejs.org"
+						target="_blank"
+						rel="noopener"
+					>
+						vuex
+					</a>
+				</li>
+				<li class="hello__item">
+					<a
+						class="hello__link"
+						href="https://github.com/vuejs/vue-devtools#vue-devtools"
+						target="_blank"
+						rel="noopener"
+					>
+						vue-devtools
+					</a>
+				</li>
+				<li class="hello__item">
+					<a
+						class="hello__link"
+						href="https://vue-loader.vuejs.org"
+						target="_blank"
+						rel="noopener"
+					>
+						vue-loader
+					</a>
+				</li>
+				<li class="hello__item">
+					<a
+						class="hello__link"
+						href="https://github.com/vuejs/awesome-vue"
+						target="_blank"
+						rel="noopener"
+					>
+						awesome-vue
+					</a>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
@@ -150,26 +180,31 @@
 	.hello {
 		text-align: center;
 
-		h1 {
+		&__title {
 			font-family: $font-brand-1;
 		}
 
-		h3 {
-			margin: 40px 0 0;
+		&__subtitle {
 			font-family: $font-brand-1;
 		}
 
-		ul {
+		&__info {
+			&:not(:first-child) {
+				margin-top: 4rem;
+			}
+		}
+
+		&__list {
 			padding: 0;
 			list-style-type: none;
 		}
 
-		li {
+		&__item {
 			display: inline-block;
 			margin: 0 10px;
 		}
 
-		a {
+		&__link {
 			color: $color-brand-1;
 		}
 	}
