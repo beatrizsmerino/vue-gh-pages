65d5c543598fa6800c57a349c5503ed07df79a06
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2022-01-06 02:58:46 +0100

    SCSS. Added variables of the fonts 'Avenir' and 'Source Sans Pro'

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,8 @@
 </script>
 
 <style lang="scss">
+	@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600&display=swap');
+
 	#app {
 		color: $color-brand-2;
 		font-family: 'Avenir', Helvetica, Arial, sans-serif;
