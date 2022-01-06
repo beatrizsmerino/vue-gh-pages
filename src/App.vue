cdd1c357061fd7d59dcb65e092ce38f92b0767bd
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2022-01-06 03:26:28 +0100

    SCSS. Added 'base' scss folder with 'reset' and 'global' styles

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,21 @@
 <style lang="scss">
 	@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600&display=swap');
 
-	#app {
-		color: $color-brand-2;
-		font-family: $font-brand-2;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-	}
+	// PARTIALS SASS
+	// =================================================
+	// Pattern 7-1
+	// https://sass-guidelin.es/#the-7-1-pattern
+	// 1. ABSTRACTS (settings that not generates code)
+	// 2. BASE (standard styles for commonly used HTML elements)
+	// 3. COMPONENTS (reusables micro modules of the widgets)
+	// 4. LAYOUTS (global wireframe with the main parts)
+	// 5. PAGES (styles for each page)
+	// 6. THEMES (change the state of colors dark or light)
+	// 7. VENDORS (external css code of the libraries and frameworks)
+
+	// BASE
+	//----------------------------------------------------------------------
+	@import '@/assets/scss/base/base-reset';
+	@import '@/assets/scss/base/base-global';
+
 </style>
