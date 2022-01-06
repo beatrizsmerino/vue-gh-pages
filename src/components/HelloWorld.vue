104b4c5a3e8df76c8bfbca54c75b1f87649aeca8
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2022-01-06 01:06:20 +0100

    REFACTOR. Format code with the npm script 'lint'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -150,15 +150,18 @@
 	h3 {
 		margin: 40px 0 0;
 	}
+
 	ul {
-		list-style-type: none;
 		padding: 0;
+		list-style-type: none;
 	}
+
 	li {
 		display: inline-block;
 		margin: 0 10px;
 	}
+
 	a {
-		color: #42b983;
+		color: #42B983;
 	}
 </style>
