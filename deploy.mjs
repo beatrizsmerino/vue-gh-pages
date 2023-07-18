9eb180d5dff58a3f3b3f78c0ac132bf1c5d1716b
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-07-18 12:02:25 +0200

    BUGFIX. Get errors on `execa` package when executing the `npm run deploy` command
    Error:
    - Error [ERR_REQUIRE_ESM]: Must use import to load ES Module: /Applications/MAMP/htdocs/vue-gh-pages/node_modules/execa/index.js
    require() of ES modules is not supported.
    - Warning: To load an ES module, set "type": "module" in the package.json or use the .mjs extension.
    - Error: Cannot find module '/Applications/MAMP/htdocs/vue-gh-pages/deploy.js'.
    
    Reason:
    - When upgrading the execa package from version 5 to version 7, errors were found due to a change in the module system used. The package now uses ECMAScript Modules (ESM) instead of CommonJS.
    
    Solution:
    - In the `deploy.js` file use `import` instead of `require` to add and use the packages.
    - Rename the file `deploy.js` to `deploy.mjs`.
    - As the file name has changed to `deploy.mjs`, modify it in the `deploy` npm script.
    
    References:
    - https://www.npmjs.com/package/execa
    - https://www.npmjs.com/package/node-emoji
    - https://stackoverflow.com/questions/43622337/using-import-fs-from-fs

--- deploy.mjs
+++ deploy.mjs
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
-const execa = require("execa");
-const fs = require("fs");
+import { execa } from "execa";
+import * as fs from "fs";
 
 // eslint-disable-next-line max-statements
 (async () => {
