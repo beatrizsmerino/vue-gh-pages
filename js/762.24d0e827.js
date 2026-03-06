50e6c4d29ca05c528b6958d1bdcbf3529501b6c9
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2026-03-06 09:18:21 +0000

    ci(deploy): build files for production in the dist folder

---
+++ js/762.24d0e827.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_gh_pages"]=self["webpackChunkvue_gh_pages"]||[]).push([[762],{762:function(e,s,t){t.r(s),t.d(s,{default:function(){return c}});var a=function(){var e=this;e._self._c;return e._m(0)},n=[function(){var e=this,s=e._self._c;return s("div",{staticClass:"about"},[s("section",{staticClass:"page__section"},[s("h1",{staticClass:"page__title page__inner"},[e._v(" This is an about page ")])])])}],u=t(656),i={},_=(0,u.A)(i,a,n,!1,null,"621b7b2e",null),c=_.exports}}]);
+//# sourceMappingURL=762.24d0e827.js.map
